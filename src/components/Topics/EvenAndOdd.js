3ca68b4b8ff41efce0176992eedcc4c3eb190e94
distoneman <distoneman@gmail.com>
2018-11-30 21:38:42 -0700

    EvenAndOdd.js functionality working

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react';
 
-class EvenAndOdds extends Component{
-    constructor(){
+class EvenAndOdds extends Component {
+    constructor() {
         super()
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: ''
+        }
     }
 
-    splitArray(){
-
+    splitArray(userInput) {
+        let evenNums = []
+        let oddNums = []
+        // console.log("split array method")
+        var userInputArr = userInput.split(',')
+        // console.log(userInputArr)
+        for (let i = 0; i < userInputArr.length; i++) {
+            if (userInputArr[i] % 2 === 0) {
+                // console.log('even')
+                evenNums.push(parseInt(userInputArr[i], 10))
+                // console.log(evenNums)
+            } else {
+                // console.log('odd')
+                oddNums.push(parseInt(userInputArr[i], 10))
+                // console.log('oddNums = ' + oddNums)
+            }
+        }
+        this.setState({
+            evenArray: evenNums,
+            oddArray: oddNums
+        })
     }
 
-    render(){
-        return(
-            <p>
-                Even and Odds Component
-            </p>
+    handleChange(val) {
+        // console.log(val)
+        this.setState({
+            userInput: val
+        })
+    }
+
+    // <button onClick={() => this.handleAddFriend()}>Add Friend</button>
+
+
+    render() {
+        return (
+            <div className="puzzleBox evenAndOddPB">
+                <h4>Evens and Odds</h4>
+                <input className="inputLine"
+                    onChange={(evt) => this.handleChange(evt.target.value)}>
+
+                </input>
+                <button className="confirmationButton"
+                    onClick={() => this.splitArray(this.state.userInput)}>
+                    Split
+                </button>
+                <span className="resultsBox">
+                    Evens:  { this.state.evenArray }
+                </span>
+                <span className="resultsBox">
+                    Odds: { this.state.oddArray }
+                </span>
+            </div>
         )
     }
 }
